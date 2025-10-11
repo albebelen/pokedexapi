6c0824e65c3a7f08dcbd9c0f7664e8a20eb26423
albebelen <albebelen@gmail.com>
2025-10-11 23:44:44 +0200

    defined the properties of a pokemon

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -2,5 +2,9 @@ namespace PokedexApi.Models;
 
 public class Pokemon
 {
-
+    public int id { get; set; };
+    public string name { get; set; };
+    public bool isLegendary { get; set; };
+    public string description { get; set; };
+    public string? habitat {get; set; }
 }
