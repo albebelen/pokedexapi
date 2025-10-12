5662cbcab2bc151f0e84fbd031f4d2f1ad67a3da
albebelen <albebelen@gmail.com>
2025-10-12 19:22:46 +0200

    implemented get endpoint

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using PokedexApi.Models;
+using Newtonsoft.Json.Linq;
 
 namespace PokedexApi.Controllers;
 
@@ -11,5 +13,28 @@ public class PokemonController : ControllerBase
     public PokemonController(ILogger<PokemonController> logger)
     {
         _logger = logger;
+    }
+
+    [HttpGet("{name}")]
+    public async Task<Pokemon> GetPokemon(string name) 
+    {
+        Pokemon pokemon;
+        string url = $"https://pokeapi.co/api/v2/pokemon-species/{name}"; // global ? 
+
+        using (HttpClient client = new HttpClient()) 
+        {
+            HttpResponseMessage res = await client.GetAsync(url);
+            var jsonRes = JObject.Parse(await res.Content.ReadAsStringAsync());
+            pokemon = new Pokemon()
+            {
+                Id = (int) jsonRes["id"],
+                Name = jsonRes["name"].ToString(),
+                Habitat = jsonRes["habitat"]["name"].ToString(),
+                Description = jsonRes["flavor_text_entries"][0]["flavor_text"].ToString(),
+                IsLegendary = (bool) jsonRes["is_legendary"]
+            };  
+        }
+
+        return pokemon;
     }
 }
